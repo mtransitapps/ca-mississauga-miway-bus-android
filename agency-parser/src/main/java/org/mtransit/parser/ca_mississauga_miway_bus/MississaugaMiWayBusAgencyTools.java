4f7c69b9889020929980f76e36e72bee04116af2
Mathieu Méa <mathieu.mea@gmail.com>
2025-12-09 08:15:37 -0500

    Update MississaugaMiWayBusAgencyTools.java

--- agency-parser/src/main/java/org/mtransit/parser/ca_mississauga_miway_bus/MississaugaMiWayBusAgencyTools.java
+++ agency-parser/src/main/java/org/mtransit/parser/ca_mississauga_miway_bus/MississaugaMiWayBusAgencyTools.java
@@ -9,7 +9,7 @@ import org.mtransit.parser.DefaultAgencyTools;
 import org.mtransit.parser.gtfs.data.GRoute;
 import org.mtransit.parser.gtfs.data.GStop;
 import org.mtransit.parser.mt.data.MAgency;
-import org.mtransit.parser.mt.data.MTrip;
+import org.mtransit.parser.mt.data.MDirection;
 
 import java.util.Arrays;
 import java.util.List;
@@ -80,8 +80,8 @@ public class MississaugaMiWayBusAgencyTools extends DefaultAgencyTools {
 	@Override
 	public List<Integer> getDirectionTypes() {
 		return Arrays.asList(
-				MTrip.HEADSIGN_TYPE_DIRECTION,
-				MTrip.HEADSIGN_TYPE_STRING
+				MDirection.HEADSIGN_TYPE_DIRECTION,
+				MDirection.HEADSIGN_TYPE_STRING
 		);
 	}
 
